3f3008939f07d9b64a24ceba15f716154875efd2
Yaroslav <yaroslav_lav1@bk.ru>
2022-11-05 21:09:05 +0300

    Project1

--- src/main/java/yar/lavr/App.java
+++ src/main/java/yar/lavr/App.java
@@ -4,11 +4,18 @@ package yar.lavr;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import yar.lavr.model.Director;
+import yar.lavr.model.Movie;
 import yar.lavr.model.Person;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        Configuration configuration = new Configuration().addAnnotatedClass(Director.class).addAnnotatedClass(Movie.class);
+
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
@@ -16,10 +23,43 @@ public class App {
         try {
             session.beginTransaction();
 
-            Person person = session.get(Person.class, 1);
 
-            System.out.println(person.getName());
-            System.out.println(person.getAge());
+            Movie movie = session.get(Movie.class,3);
+            Director director = movie.getOwner();
+
+            session.remove(movie);
+
+            movie.setOwner(null); //???
+            director.getMovies().remove(movie);
+
+
+
+
+//            Director newDirector = new Director("Director from project1", 1950);
+//            Movie newMovie = new Movie("Movie2 from project1", 1980,newDirector);
+//
+//            newDirector.setMovies(new ArrayList<>(Collections.singletonList(newMovie)));
+//
+//            session.save(newDirector);
+//            session.save(newMovie);
+
+//            Director director=session.get(Director.class,1);
+//            Movie newMovie = new Movie("New movie from project", 1999,director);
+//
+//            director.getMovies().add(newMovie);
+//            session.save(newMovie);
+
+
+//            Movie movie=session.get(Movie.class,3);
+//            System.out.println(movie);
+//
+//            Director director = movie.getOwner();
+
+//            Director director = session.get(Director.class,1);
+//            System.out.println(director);
+//
+//            List<Movie> movies = director.getMovies();
+//            System.out.println(movies);
 
             session.getTransaction().commit();
         } finally {
---
+++ src/main/java/yar/lavr/model/Director.java
@@ -0,0 +1,71 @@
+package yar.lavr.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "Director")
+public class Director {
+
+    @Id
+    @Column(name = "director_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int directorId;
+
+    @Column(name = "name")
+    private String name;
+    @Column(name = "age")
+    private int age;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Movie> movies;
+
+    public Director() {
+    }
+
+    public Director(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getDirectorId() {
+        return directorId;
+    }
+
+    public void setDirectorId(int direcotr_id) {
+        this.directorId = direcotr_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
+
+    @Override
+    public String toString() {
+        return "Director{" +
+                "directorId=" + directorId +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
